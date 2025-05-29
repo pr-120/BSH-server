eb74ad6bc6209572443acd2e155e0ddd43666fd0
Patrik <patrikrosenkranz@gmail.com>
2025-05-29 16:32:01 +0200

    feat: implemented LOF classifier

--- server/environment/anomaly_detection/constructor.py
+++ server/environment/anomaly_detection/constructor.py
@@ -1,4 +1,5 @@
 from pyod.models.iforest import IForest
+from pyod.models.lof import LOF
 
 from environment.anomaly_detection.advanced_preprocessor import AdvancedPreprocessor
 from environment.anomaly_detection.autoencoder import AutoEncoder
@@ -9,7 +10,7 @@ from environment.state_handling import get_prototype
 # ========================================
 # ==========   CONFIG   ==========
 # ========================================
-CONTAMINATION_FACTOR = 0.05
+CONTAMINATION_FACTOR = 0.01
 
 # ========================================
 # ==========   GLOBALS   ==========
@@ -44,7 +45,8 @@ def get_classifier():
     if not CLASSIFIER:
         proto = get_prototype()
         if proto in ["1", "2", "3", "4", "5", "6", "7", "8", "10", "98", "99"]:
-            CLASSIFIER = IForest(random_state=42, contamination=CONTAMINATION_FACTOR)
+            #CLASSIFIER = IForest(random_state=42, contamination=CONTAMINATION_FACTOR)
+            CLASSIFIER = LOF(n_neighbors=1500, contamination=CONTAMINATION_FACTOR)
         elif proto in ["9"]:
             CLASSIFIER = AutoEncoder(encoding_dim=[40, 20, 10, 20, 40], random_state=42,
                                      outlier_percentage=CONTAMINATION_FACTOR)
