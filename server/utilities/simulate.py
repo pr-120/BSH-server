2231444835fd910e7934de939a256f4a79c525ce
Patrik <patrikrosenkranz@gmail.com>
2025-06-04 16:29:11 +0200

    fix: fixed wrong folder path

--- server/utilities/simulate.py
+++ server/utilities/simulate.py
@@ -2,7 +2,7 @@ import json
 import os
 import random
 
-from environment.settings import TRAINING_CSV_FOLDER_PATH, FINGERPRINT_FOLDER_PATH
+from environment.settings import FINGERPRINT_FOLDER_PATH
 from environment.state_handling import get_storage_path, is_multi_fp_collection, set_rw_done
 from utilities.metrics import write_metrics_to_file
 
@@ -18,7 +18,7 @@ AVERAGE_RATES = {  # calculated by auxiliary script ´find_avg_rate.py´
 
 
 def simulate_sending_fp(config_num):
-    config_fp_dir = os.path.join(FINGERPRINT_FOLDER_PATH, "infected-c{}".format(config_num))
+    config_fp_dir = os.path.join(FINGERPRINT_FOLDER_PATH, "training", "infected-c{}".format(config_num))
     fp_files = os.listdir(config_fp_dir)
     with open(os.path.join(config_fp_dir, random.choice(fp_files))) as fp_file:
         # print("SIM: fp", fp_file.name)
